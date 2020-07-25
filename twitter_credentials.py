c80ec056146b63c8b7bbbfc78defe9ec91c1e4eb
reenazhang <55249550+reenazhang@users.noreply.github.com>
2020-07-25 15:10:32 -0400

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,7 +0,0 @@
-# vars containing user credentials to access tweepy
-ACCESS_TOKEN = "2924979434-BsmyKnoNfvEkClt9JhjL0bwaljCWE8QgW5Cjk6F"
-ACCESS_TOKEN_SECRET = "JjshIKcqTzPf0xWCyo6gcxJbsXQyQz3iuSM7Yb457lpkP"
-
-# Note: aka API Key
-CONSUMER_KEY = "BtZe6bElU9yHjmlxnWQzsfpst"
-CONSUMER_SECRET = "62Qi9OS6u5YUHHgPJpO7O1ID2KcPcyfxl7Bew3pewxVe8iXOB9"
